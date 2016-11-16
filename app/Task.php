d39a34970c2db7692ccd0e2abd5169ee76f38e92
Mario Pérez Esteso <mario@geekytheory.com>
2016-11-16 23:10:52 +0000

    Add task model

---
+++ app/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'tasks';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'task',
+    ];
+}
