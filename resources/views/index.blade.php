ae177e415424abe54b5e26e2fe4abd8e5bbced01
Mario Pérez Esteso <mario@geekytheory.com>
2016-11-16 23:42:22 +0000

    Add tasks view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    </head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <div class="todolist not-done">
+                    <h1>Tasks</h1>
+                    <input type="text" class="form-control add-todo" placeholder="Add task">
+                    <hr>
+                    <ul id="sortable" class="list-unstyled">
+                        <li>
+                            <label>
+                                <a href="#" class="btn btn-danger btn-sm">
+                                    <span class="glyphicon glyphicon-trash"></span>    
+                                </a>
+                                Take out the trash
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
